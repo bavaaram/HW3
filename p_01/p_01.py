67a897b242498de3f85c7af3494919ffee15433f
bavaaram <itsjustabavaar@gmail.com>
2023-05-07 17:16:00 +0330

    Add Required primary codes for problem 01 python scripts.
    1. add a dictionary called "nums" that contains word number and integers
    couples from (1 & one) to (19 & nineteen).
    
    2. Open Zen.txt file and read whole file and assign that to a variable
    called "text".
    
    3. split file content by "\n", this means that each line of file content
    now is a item in out list called "text_line".
    
    4. iterate around each line with a "for loop" (line 14)
    
    5. now split each line contents by "space" to reach each word that exist in every line at every iterate.
    
    6. Now with a map function, check whether there are letters between one and nineteen in each line and if it exists, we will replace it with its numerical equivalent that is in the dictionary. The implementation of the checking function and replacement with the lambda function has been done.
    
    7. Then join the words in each line by "space" with .join(list) method
       and join each line of file content by "\n" with .join(list) (in lines
       17, 18) actually this two lines is recursive path :)
    
    8. Finally open a file called "NewZen.txt" and write our work into it :)

---
+++ p_01/p_01.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+nums = {
+        "one": "1", "two": "2", "three": "3", "four": "4",
+        "five": "5", "six": "6", "seven": "7", "eight": "8",
+        "nine": "9", "ten": "10", "eleven": "11", "twelve": "12",
+        "thirteen": "13", "fourteen": "14", "fifteen": "15", "sixteen": "16",
+        "seventeen": "17", "eighteen": "18", "nineteen": "19"
+}
+
+with open("Zen.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+text_line, emp = text.split("\n"), []
+for line in text_line:
+    char = line.split()
+    char = list(map(lambda x: nums[x] if x in nums else x, char))
+    emp.append(" ".join(char))
+TEXT = "\n".join(emp)
+
+with open("NewZen.txt", "w+", encoding="utf-8") as t:
+    t.write(TEXT)
