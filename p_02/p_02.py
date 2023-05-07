0f12aa4d845a125dd04ac694c4b2c9dd271597f8
bavaaram <itsjustabavaar@gmail.com>
2023-05-07 18:34:39 +0330

    Add Required codes for LBYL Style in p_02.py python script I Implement division function with LBYL Style. 1. first of all, I put a condition for second number, if it is zero. function print an massage that you divided by zero and answer is infinite. 2. Then I add another condition that check input types, if they are strings, function print a massage that inputs are invelid and try again 3. finally if all above conditions not occured, function calculate the division result and print it.

--- p_02/p_02.py
+++ p_02/p_02.py
@@ -9,12 +9,22 @@ def div_eafp(n_1, n_2):
     Implemented with EAFP style
     """
     try:
-        print(float(n_1) / float(n_2))
+        print(f"Answer is: {float(n_1) / float(n_2)}")
     except ZeroDivisionError:
         print(f"Division by Zero: {math.inf}")
     except ValueError:
         print("Invalid Inputs. Please Try again.")
 
 
-num1, num2 = input().split()
-div_eafp(num1, num2)
+# LBYL Style
+def div_lbyl(n_1, n_2):
+    """
+    This function divide two numbers./
+    Implemented with LBYL style
+    """
+    if float(n_2) == 0:
+        print(f"Division by Zero: {math.inf}")
+    elif isinstance(n_1, str) or isinstance(n_2, str):
+        print("Invalid Input. Please Try again.")
+    else:
+        print(f"Answer is: {float(n_1) / float(n_2)}")
