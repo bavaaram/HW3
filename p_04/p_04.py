487e594adc94c50dd7cb0724c46056515d41c5b7
bavaaram <itsjustabavaar@gmail.com>
2023-05-08 18:25:05 +0330

    Add some codes for calculating age in seconds 1. with day, month and year variable that i taking them from user, i    make a date instance (line 15)
    2. make another instance called "now" that represents now time.
    
    3. now calculate time delta between birthdate and now and finally
       convert it with .total_seconds()

--- p_04/p_04.py
+++ p_04/p_04.py
@@ -9,3 +9,12 @@ year, month, day = input("Please Enter your Birth date\
 in format yyyy/mm/dd: ").split("/")
 year, month, day = int(year), int(month), int(day)
 print("\n************************************************\n")
+
+
+# Age in Seconds
+org = datetime(year, month, day)
+now = datetime.now()
+delta1 = (now - org).total_seconds()
+print(f"You are {delta1} seconds old")
+print("\n************************************************\n")
+
