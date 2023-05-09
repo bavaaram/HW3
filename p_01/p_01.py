56dd46ce37fdea766e15ca46726220e8332bd983
bavaaram <itsjustabavaar@gmail.com>
2023-05-09 04:53:07 +0330

    Add two full paths of Zen.txt and NewZen.txt text files. Because if we execute p_01.py python scripts in other pwd, we got an error in reading Zen.txt and NewZen.txt will be created in our pwd.

--- p_01/p_01.py
+++ p_01/p_01.py
@@ -7,8 +7,9 @@ nums = {
         "thirteen": "13", "fourteen": "14", "fifteen": "15", "sixteen": "16",
         "seventeen": "17", "eighteen": "18", "nineteen": "19"
 }
-
-with open("Zen.txt", "r", encoding="utf-8") as f:
+PATH1 = "/home/bavaar/Desktop/Python_Projects/Bootcamp Projects/HW3/p_01/Zen.txt"
+PATH2 = "/home/bavaar/Desktop/Python_Projects/Bootcamp Projects/HW3/p_01/NewZen.txt"
+with open(PATH1, "r", encoding="utf-8") as f:
     text = f.read()
 text_line, emp = text.split("\n"), []
 for line in text_line:
@@ -17,5 +18,5 @@ for line in text_line:
     emp.append(" ".join(char))
 TEXT = "\n".join(emp)
 
-with open("NewZen.txt", "w+", encoding="utf-8") as t:
+with open(PATH2, "w+", encoding="utf-8") as t:
     t.write(TEXT)
