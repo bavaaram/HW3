39691f8dc71e66309585a6c0befb32e60623cdc4
bavaaram <itsjustabavaar@gmail.com>
2023-05-09 05:00:22 +0330

    Restoring p_01.py to previous commit, because this full path is not valid on other devices.

--- p_01/p_01.py
+++ p_01/p_01.py
@@ -7,9 +7,7 @@ nums = {
         "thirteen": "13", "fourteen": "14", "fifteen": "15", "sixteen": "16",
         "seventeen": "17", "eighteen": "18", "nineteen": "19"
 }
-PATH1 = "/home/bavaar/Desktop/Python_Projects/Bootcamp Projects/HW3/p_01/Zen.txt"
-PATH2 = "/home/bavaar/Desktop/Python_Projects/Bootcamp Projects/HW3/p_01/NewZen.txt"
-with open(PATH1, "r", encoding="utf-8") as f:
+with open("Zen.txt", "r", encoding="utf-8") as f:
     text = f.read()
 text_line, emp = text.split("\n"), []
 for line in text_line:
@@ -18,5 +16,5 @@ for line in text_line:
     emp.append(" ".join(char))
 TEXT = "\n".join(emp)
 
-with open(PATH2, "w+", encoding="utf-8") as t:
+with open("NewZen.txt", "w+", encoding="utf-8") as t:
     t.write(TEXT)
